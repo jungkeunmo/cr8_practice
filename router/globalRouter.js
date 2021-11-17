395282a6e3aa526fa111f37122e3654c4a154ac5
“jungkeunmo“ <abc59684495@gmail.com>
2021-11-17 21:26:12 +0900

    cr_8

---
+++ router/globalRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const db = require("../db");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    const selectQuery = `
+        
+    `;
+});
